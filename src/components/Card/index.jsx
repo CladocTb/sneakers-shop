f7a69c6e6222916b9a3e4190520439dee4253feb
CladocTb <vadumalatka7@gmail.com>
2024-10-16 14:50:03 +0200

    lesson-4

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,24 +1,32 @@
+import React from "react";
 import styles from "./Card.module.scss";
-export function Card(props) {
-  const onClickButton = () => {
-    alert(props.title);
+export function Card({ title , imageUrl, price, onFavorite, onPlus}) {
+  const [isAdded, setIsAdded] = React.useState(false);
+
+  const onClickPlus = () => {
+    onPlus({ title, imageUrl, price});
+    setIsAdded( !isAdded);
   };
 
   return (
     <div className={styles.card}>
-      <div className={styles.favorite}>
+      <div className={styles.favorite} onClick={onFavorite}>
         <img src="/img/heard-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-      <h5>{props.title}</h5>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column ">
           <p>Цена</p>
-          <b>{props.price}</b>
+          <b>{price}</b>
         </div>
-        <button className="button" onClick={props.onClick}>
-          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-        </button>
+
+        <img
+          className={styles.plus}
+          onClick={onClickPlus}
+          src={isAdded ? "/img/btn-cheked.svg" : "/img/btn-plus.svg"}
+          alt="Plus"
+        />
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,22 @@
-export function Header () {
-    return (
-        <header className="d-flex justify-between aling-center p-40">
-        <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" />
-          <div>
-            <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Магазин лучших кроссовок</p>
-          </div>
+export function Header(props) {
+  return (
+    <header className="d-flex justify-between aling-center p-40">
+      <div className="d-flex align-center">
+        <img width={40} height={40} src="/img/logo.png" />
+        <div>
+          <h3 className="text-uppercase">React Sneakers</h3>
+          <p className="opacity-5">Магазин лучших кроссовок</p>
         </div>
-        <ul className="d-flex">
-          <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.svg" />
-            <span>1205 грн.</span>
-          </li>
-          <li>
-            <img width={18} height={18} src="/img/user.svg" />
-          </li>
-        </ul>
-      </header>
-    );
+      </div>
+      <ul className="d-flex">
+        <li onClick={props.onClickCart} className="mr-30 cu-p">
+          <img width={18} height={18} src="/img/cart.svg" />
+          <span>1205 грн.</span>
+        </li>
+        <li>
+          <img width={18} height={18} src="/img/user.svg" />
+        </li>
+      </ul>
+    </header>
+  );
 }
